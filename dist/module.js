095521c7a15460bae92bc0bb17f84002c47bfa14
mdwitr0 <mdwit0r@gmail.com>
2022-08-14 20:44:18 +0200

    add output interface

---
+++ dist/module.js
@@ -0,0 +1,9 @@
+const $a284fa37cee6c3a8$export$a2fe3c907bb1ef90 = (curlStr)=>{
+    return {};
+};
+
+
+
+
+export {$a284fa37cee6c3a8$export$a2fe3c907bb1ef90 as curlToObject};
+//# sourceMappingURL=module.js.map
---
+++ src/curl-to-object.ts
@@ -0,0 +1,5 @@
+import {IOutput} from "./curl-to-object.interface";
+
+export const curlToObject = <D = any>(curlStr: string): IOutput<D> => {
+    return {}
+}
