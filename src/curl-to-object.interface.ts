60f29d2b5ec4ebc0a7c371dbdcadf5424a251729
mdwitr0 <mdwit0r@gmail.com>
2022-08-15 22:40:30 +0200

    remove lowerCase methods

--- src/curl-to-object.interface.ts
+++ src/curl-to-object.interface.ts
@@ -1,66 +1,56 @@
 export type HeaderTitle =
-    | 'User-Agent'
-    | 'Accept'
-    | 'Accept-Encoding'
-    | 'Accept-Language'
-    | 'Cache-Control'
-    | 'Connection'
-    | 'Content-Length'
-    | 'Content-Type'
-    | 'Cookie'
-    | 'Host'
-    | 'Origin'
-    | 'Referer'
-    | 'Upgrade-Insecure-Requests'
-    | 'X-Requested-With';
+  | "User-Agent"
+  | "Accept"
+  | "Accept-Encoding"
+  | "Accept-Language"
+  | "Cache-Control"
+  | "Connection"
+  | "Content-Length"
+  | "Content-Type"
+  | "Cookie"
+  | "Host"
+  | "Origin"
+  | "Referer"
+  | "Upgrade-Insecure-Requests"
+  | "X-Requested-With";
 
 export type Method =
-    | 'get'
-    | 'GET'
-    | 'delete'
-    | 'DELETE'
-    | 'head'
-    | 'HEAD'
-    | 'options'
-    | 'OPTIONS'
-    | 'post'
-    | 'POST'
-    | 'put'
-    | 'PUT'
-    | 'patch'
-    | 'PATCH'
-    | 'purge'
-    | 'PURGE'
-    | 'link'
-    | 'LINK'
-    | 'unlink'
-    | 'UNLINK';
+  | "GET"
+  | "DELETE"
+  | "HEAD"
+  | "OPTIONS"
+  | "POST"
+  | "PUT"
+  | "PATCH"
+  | "PURGE"
+  | "LINK"
+  | "UNLINK";
 
 export type Headers = Record<HeaderTitle | string, string>;
 
 export interface ICredentials {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export interface IProxy {
-    host: string;
-    port: number;
-    auth?: {
-        username: string;
-        password: string;
-    };
-    protocol?: string;
+  host: string;
+  port: number;
+  auth?: {
+    username: string;
+    password: string;
+  };
+  protocol?: string;
 }
 
 export interface IOutput<D = any> {
-    url?: string;
-    method?: Method;
-    baseURL?: string;
-    headers?: Headers;
-    params?: any;
-    data?: D;
-    timeout?: number;
-    auth?: ICredentials;
-    proxy?: IProxy | false;
+  url?: string;
+  method?: Method;
+  baseURL?: string;
+  headers?: Headers;
+  params?: any;
+  data?: D;
+  timeout?: number;
+  auth?: ICredentials;
+  proxy?: IProxy | false;
 }
