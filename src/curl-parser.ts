dde68bf68627d55cb1442e09a22dceb55f0453a5
mdwitr0 <mdwit0r@gmail.com>
2022-08-19 15:07:14 +0200

    fix: not valid quotes pattern
    Signed-off-by: mdwitr0 <mdwit0r@gmail.com>

--- src/curl-parser.ts
+++ src/curl-parser.ts
@@ -1,16 +1,16 @@
 import {URL} from 'url'
 import {UrlObject} from "./interface";
-import {PATTERNS} from "./regexp-patterns"
+import {ARGS_PATTERN, QUOTES_PATTERN, URL_PATTERN} from "./regexp-patterns"
 
 const split = (str: string): string[] => {
     if (!str) return [];
 
-    const args = str.match(PATTERNS.ARGS);
-    return args?.map((i) => i.trim().replace(PATTERNS.QUOTES, "")) || [];
+    const args = str.match(ARGS_PATTERN);
+    return args?.map((i) => i.trim().replace(QUOTES_PATTERN, "")) || [];
 };
 
 const getUrl = <D>(args: string[]): UrlObject | null => {
-    const foundUrl = args.find((a) => PATTERNS.URL.test(a));
+    const foundUrl = args.find((a) => URL_PATTERN.test(a));
 
     if (foundUrl) {
         const obj = new URL(foundUrl);
